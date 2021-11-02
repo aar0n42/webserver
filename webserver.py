1221fc4f66e76915e8bd1264f748326a8c55b306
aar0n42 <91214823+aar0n42@users.noreply.github.com>
2021-11-02 10:38:59 +0100

    Update webserver.py

--- webserver.py
+++ webserver.py
@@ -10,7 +10,7 @@ def hello():
     html = "<h3>Hello {name}!</h3>" \
            "<b>Hostname:</b> {hostname}<br/>"
            
-    return html.format(name="Manoj Group 3", hostname=socket.gethostname())           
+    return html.format(name="Aaron Gruppe 0", hostname=socket.gethostname())           
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
